e613c428e9e3d413c8d7419c902cfc865dcf8bde
Jose C Ponce <pepote5391@gmail.com>
2017-06-02 10:15:08 -0400

    Now fully configuring the web application

--- docker/entrypoint.bash
+++ docker/entrypoint.bash
@@ -4,4 +4,9 @@ echo "Waiting for mongo to be ready"
 #no way to really know how long mongo will take to be ready
 sleep 120
 
+#configure the application
+sed -i "s/app.set(\"host\",.*;/app.set(\"host\", \"$VIP_WEB.ignorelist.com\");/" "/Code/server.js"
+sed -i "s/'callbackURL':.*/'callbackURL':'htt\/\/$VIP_WEB:3000\/auth\/google\/callback'/" "/Code/api/config/auth.js"
+sed -i "s/'database':.*/'database':'mongodb:\/\/mongo\/admin',/" "/Code/api/config/config"
+
 node /Code/server.js
