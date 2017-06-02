f31edbedfc26608d04f7d2599fcf4978a8db92cb
Jose C Ponce <pepote5391@gmail.com>
2017-06-02 15:26:17 -0400

    Typos are fun.

--- docker/entrypoint.bash
+++ docker/entrypoint.bash
@@ -7,7 +7,7 @@ sleep 120
 #configure the application
 sed -i "s/app.set(\"host\",.*;/app.set(\"host\", \"$VIP_WEB.ignorelist.com\");/" "/Code/server.js"
 sed -i "s/'callbackURL':.*/'callbackURL':'http:\/\/$VIP_WEB.ignorelist.com\/auth\/google\/callback',/" "/Code/api/config/auth.js"
-sed -i "s/'clientID':.*/'clientID':'119517664477-gcj01c4hrda64jlhihfmoq3ponn83nd8.apps.googleusercontent.com',/" "/Code/api/config/auth.js"
+sed -i "s/'clientID'.*/'clientID':'119517664477-gcj01c4hrda64jlhihfmoq3ponn83nd8.apps.googleusercontent.com',/" "/Code/api/config/auth.js"
 sed -i "s/'clientSecret':.*/'clientSecret':'yi_x_6QiScR2nq1M8e2L8nLx',/" "/Code/api/config/auth.js"
 sed -i "s/'database':.*/'database':'mongodb:\/\/mongo\/admin',/" "/Code/api/config/config"
 
