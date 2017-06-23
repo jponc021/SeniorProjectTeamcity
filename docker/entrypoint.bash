74be4c786e33ad47433207dec7b8c52aac9fd9ed
Jose C Ponce <pepote5391@gmail.com>
2017-06-23 13:57:19 -0400

    Configuring the external port in the api/config/config folder.

--- docker/entrypoint.bash
+++ docker/entrypoint.bash
@@ -10,6 +10,7 @@ sed -i "s/'callbackURL':.*/'callbackURL':'http:\/\/$VIP_WEB.ignorelist.com\/auth
 sed -i "s/'clientID'.*/'clientID':'119517664477-gcj01c4hrda64jlhihfmoq3ponn83nd8.apps.googleusercontent.com',/" "/Code/api/config/auth.js"
 sed -i "s/'clientSecret':.*/'clientSecret':'yi_x_6QiScR2nq1M8e2L8nLx',/" "/Code/api/config/auth.js"
 sed -i "s/'database':.*/'database':'mongodb:\/\/mongo\/admin',/" "/Code/api/config/config"
+sed -i "s/'externalPort':.*/'externalPort':80,/" "/Code/api/config/config"
 
 cd /Code/deployment
 npm install
